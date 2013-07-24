eff6eeee17187080c0ba8ffd6836b87397d0f3f2
Newname <newemail>
2013-07-24 10:51:48 -0400

    Add BDDAppDelegateSpec.mm

---
+++ Specs/BDDAppDelegateSpec.mm
@@ -0,0 +1,16 @@
+#import "BDDAppDelegate.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(BDDAppDelegateSpec)
+
+describe(@"BDDAppDelegate", ^{
+    __block BDDAppDelegate *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
