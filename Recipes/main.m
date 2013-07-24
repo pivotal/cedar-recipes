b55e66d2d99efcdcaa8d56c7e7c799e0ae994d5f
Alex Basson and Sam Coward <pair+abasson+scoward@pivotallabs.com>
2013-07-24 10:47:36 -0400

    Initial Commit

---
+++ Recipes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Recipes
+//
+//  Created by pivotal on 7/24/13.
+//  Copyright (c) 2013 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BDDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BDDAppDelegate class]));
+    }
+}
