92018c2932b5b1e74d7031c436aaa69f37bef351
Andy Pliszka and Joe Masilotti <pair+apliszka+jmasilotti@pivotallabs.com>
2013-07-26 15:04:07 -0400

    Add first failing test to BDDAppDelegateSpec

--- Specs/BDDAppDelegateSpec.mm
+++ Specs/BDDAppDelegateSpec.mm
@@ -6,10 +6,20 @@ using namespace Cedar::Doubles;
 SPEC_BEGIN(BDDAppDelegateSpec)
 
 describe(@"BDDAppDelegate", ^{
-    __block BDDAppDelegate *model;
-
+    __block BDDAppDelegate *delegate; // 1.
+    
     beforeEach(^{
-
+        delegate = [[[BDDAppDelegate alloc] init] autorelease]; // 2.
+    });
+    
+    context(@"when the app is finished loading", ^{ // 3.
+        beforeEach(^{ // 4.
+            [delegate application:nil didFinishLaunchingWithOptions:nil];
+        });
+        
+        it(@"should display a table view", ^{
+            delegate.window.rootViewController should be_instance_of([UITableViewController class]); // 5.
+        });
     });
 });
 
