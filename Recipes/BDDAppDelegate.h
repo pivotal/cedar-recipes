499f0c0342988282d0f6ab7b3bff0ecc663fc7e4
Andy Pliszka and Joe Masilotti <pair+apliszka+jmasilotti@pivotallabs.com>
2013-07-26 15:05:35 -0400

    Initial Commit

---
+++ Recipes/BDDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BDDAppDelegate.h
+//  Recipes
+//
+//  Created by pivotal on 7/24/13.
+//  Copyright (c) 2013 Pivotal Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
